9f0c929ffdbe322e308f47b5c8b8502f35aa833b
=kanhaiyaa <kanhaiyaji14@gmail.com>
2021-01-16 23:53:54 +0530

    Day 5 Problem Solved

---
+++ Day-5/1. reverse_sentence_back_to_front.py
@@ -0,0 +1,16 @@
+def reverse_sentence(my_str):
+
+  """
+  This Functin reverse a given sentence(string)
+
+  Steps:
+
+  Split the String around Spaces
+
+  then reverse the list
+
+  then join  around spaces
+
+  """
+
+  return ' '.join(reversed(my_str.split()))
